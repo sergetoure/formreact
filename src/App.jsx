4f45cfa66d7f2d3fb6ca903bafdb68524e70a8c9
Serge Toure <26191503+sergetoure09@users.noreply.github.com>
2025-06-01 17:40:08 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import DessertList from "./DessertList";
+import { useReducer, useRef } from "react";
 
 const desserts = [
   {
@@ -23,12 +24,70 @@ const desserts = [
     createdAt: "2022-01-04",
   },
 ];
+const reducer = (state, action) => {
+  if (action.type === "input") {
+    if (action.e.target.id === "username") {
+      return { ...state, username: action.e.target.value };
+    } else if (action.e.target.id === "password") {
+      return { ...state, password: action.e.target.value };
+    }
+  }
+};
+const initialState = {
+  username: "",
+  password: "",
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const fileRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const uploadFile = fileRef.current.files;
+    console.log(uploadFile);
+    console.log(state);
+  };
+
   return (
     <div className="App">
-      <h2>List of low calorie desserts:</h2>
-      <DessertList data={desserts} />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">username: </label>
+          <input
+            value={state.username}
+            onChange={(e) => dispatch({ type: "input", e: e })}
+            type="text"
+            id="username"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">password: </label>
+          <input
+            value={state.password}
+            onChange={(e) => dispatch({ type: "input", e: e })}
+            type="password"
+            id="password"
+          />
+        </div>
+        <div>
+          <label htmlFor="">upload:</label>
+          <input
+            ref={fileRef}
+            type="file"
+            name="file"
+            id="file"
+            multiple={true}
+          />
+        </div>
+        <button disabled={!state.username || !state.password} type="submit">
+          submit
+        </button>
+      </form>
+      <p>{state.username}</p>
+      <p>{state.password}</p>
     </div>
   );
 }
